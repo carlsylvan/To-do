36e8a0c60754fa83b0d4780aab899d3ce176ade7
carlsylvan <carlsylvan@gmail.com>
2022-11-05 17:19:26 +0100

    changed variable

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-var listItems = document.getElementsByTagName("li");
-var i = 0;
+let listItems = document.getElementsByTagName("li");
+let addButton = document.getElementById("addButton");
+let i = 0;
 
-document.getElementById("addButton").addEventListener("click", addListItem); 
+addButton.addEventListener("click", addListItem); 
 
 function addListItem() {
-    console.log("klickade");
     let li = document.createElement("li");
     let inputValue = document.getElementById("myInput").value;
     li.innerHTML = inputValue;
@@ -16,15 +16,15 @@ function addListItem() {
 }
 
 
-for (let i = 0; i < listItems.length; i++) {
-    let span = document.createElement("span");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    listItems[i].appendChild(span);
-}
+// for (let i = 0; i < listItems.length; i++) {
+//     let span = document.createElement("span");
+//     let txt = document.createTextNode("\u00D7");
+//     span.className = "close";
+//     span.appendChild(txt);
+//     listItems[i].appendChild(span);
+// }
 
-function removeListItem() {
-    var span = this.parentElement;
-    span.style.display = "none";
-}
+// function removeListItem() {
+//     var span = this.parentElement;
+//     span.style.display = "none";
+// }
