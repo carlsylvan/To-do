e6e166f49d119fc116515803eececef5baca0e35
carlsylvan <carlsylvan@gmail.com>
2022-11-05 17:53:26 +0100

    started remove function

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,30 @@
 let listItems = document.getElementsByTagName("li");
 let addButton = document.getElementById("addButton");
+let removeButton = document.getElementById("remove");
 let i = 0;
 
 addButton.addEventListener("click", addListItem); 
 removeButton.addEventListener("click", removeListItem);
 
 function addListItem() {
-    let li = document.createElement("li");
-    let removeButton = document.createElement("button");
+    let newLi = document.createElement("li");
+    let newRemove = document.createElement("button");
     let inputValue = document.getElementById("myInput").value;
-    removeButton.innerHTML = "Ta bort";
-    removeButton.id = "remove"
-    li.innerHTML = inputValue;
+    newRemove.innerHTML = "Ta bort";
+    newRemove.id = "remove"
+    newLi.innerHTML = inputValue;
+    newLi.id = "newTask";
     if (inputValue === "") {
         alert("Write a new task!");
     } else {
-        document.getElementById("taskList").appendChild(li);
-        document.getElementById("taskList").appendChild(removeButton);
+        document.getElementById("taskList").appendChild(newLi);
+        document.getElementById("newTask").appendChild(newRemove);
     }
 }
 
 function removeListItem() {
-    document.getElementById("newtask").remove;
+    document.getElementById("newtask").style.display = "none";
+    console.log("klick");
 }
 
 
