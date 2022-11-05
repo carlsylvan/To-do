fddcb5d2f635d6f31b0136133209a54b508c497f
carlsylvan <carlsylvan@gmail.com>
2022-11-05 11:26:36 +0100

    added addListItem function

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,30 @@
-let toDoList = []
+var listItems = document.getElementsByTagName("li");
+var i = 0;
 
-class ListItem {
-    constructor(taskName, taskDescription) {
-        this.taskName = taskName;
-        this.taskDescription = taskDescription;
+document.getElementById("addButton").addEventListener("click", addListItem); 
+
+function addListItem() {
+    console.log("klickade");
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("myInput").value;
+    li.innerHTML = inputValue;
+    if (inputValue === "") {
+        alert("Write a new task!");
+    } else {
+        document.getElementById("taskList").appendChild(li);
     }
 }
 
 
-for (let i = 0; i < 4; i++) {
-    let newListItem = new ListItem ("Dammsuga", "Dammsug alla golvytor");
-    toDoList.push(newListItem);
-} 
+for (let i = 0; i < listItems.length; i++) {
+    let span = document.createElement("span");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    listItems[i].appendChild(span);
+}
 
-
-console.log(toDoList);
+function removeListItem() {
+    var span = this.parentElement;
+    span.style.display = "none";
+}
