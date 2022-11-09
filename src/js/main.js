6a949a6c4f035cf21a05e4bbdbc6200bdd07d205
carlsylvan <carlsylvan@gmail.com>
2022-11-09 14:44:12 +0100

    added updateHtml func

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,9 @@ import { Task } from "./models/task"
 let task1 = new Task(1, "Hugga ved", "unfinished");
 let task2 = new Task(2, "Ladda kaminen", "unfinished");
 let task3 = new Task(3, "Tänd brasan", "unfinished");
+let task4 = new Task(4, "Lägg till en grej", "unfinished");
 
-let listItems = [task1, task2, task3];
+let listItems = [task1, task2, task3, task4];
 
 let addButton = document.getElementById("addButton");
 
@@ -14,6 +15,9 @@ function loadToLS() {
     localStorage.setItem("listItems", JSON.stringify(listItems));
 };
 
+loadToLS();
+createHtml();
+
 function loadFromLS() {
     let itemsFromLs = JSON.parse(localStorage.getItem("listItems"));
     listItems = itemsFromLs.map((mappedList) => {
@@ -21,7 +25,7 @@ function loadFromLS() {
     })
     
 }
-
+loadFromLS();
 function updateHtml() {
     loadFromLS();
     for (let i = 0; i < listItems.length; i++) {
@@ -33,6 +37,7 @@ function updateHtml() {
     
         newRemove.addEventListener("click", () => newLi.parentNode.removeChild(newLi));
         newRemove.addEventListener("click", () => listItems[i].status = "removed");
+        newRemove.addEventListener("click", () => loadToLS());
     
         newLi.innerHTML = listItems[i].taskDescription;
         newLi.id = "task" + listItems[i].taskId;
@@ -41,9 +46,30 @@ function updateHtml() {
         document.getElementById("task" + listItems[i].taskId).appendChild(newRemove);
         console.log(listItems);
     }
-    }
+}
 
-    function addListItem() {
+function createHtml() {
+    for (let i = 0; i < listItems.length; i++) {
+        let newLi = document.createElement("li");
+        let newRemove = document.createElement("button");
+    
+        newRemove.innerHTML = "Ta bort";
+        newRemove.id = "remove" + (listItems[i].taskId);
+    
+        newRemove.addEventListener("click", () => newLi.parentNode.removeChild(newLi));
+        newRemove.addEventListener("click", () => listItems[i].status = "removed");
+        newRemove.addEventListener("click", () => loadToLS());
+    
+        newLi.innerHTML = listItems[i].taskDescription;
+        newLi.id = "task" + listItems[i].taskId;
+    
+        document.getElementById("taskList").appendChild(newLi);
+        document.getElementById("task" + listItems[i].taskId).appendChild(newRemove);
+        console.log(listItems);
+    }
+}
+
+function addListItem() {
         let inputValue = document.getElementById("myInput").value;
     
         newListItem = new Task ((listItems.length + 1), inputValue, "unfinished");
@@ -61,6 +87,7 @@ function updateHtml() {
             
                 newRemove.addEventListener("click", () => newLi.parentNode.removeChild(newLi));
                 newRemove.addEventListener("click", () => listItems[i].status = "removed");
+                newRemove.addEventListener("click", () => updateHtml());
             
                 newLi.innerHTML = listItems[i].taskDescription;
                 newLi.id = "task" + listItems[i].taskId;
@@ -69,7 +96,7 @@ function updateHtml() {
                 document.getElementById("task" + listItems[i].taskId).appendChild(newRemove);
             }
         }   
-    }
+}
 // let listItemsObj = JSON.parse(listItemsFromLs);
 
 // for (let i = 0; i < listItems.length; i++) {
