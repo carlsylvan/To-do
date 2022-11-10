2b28f6bcb75b9242a09c3ade23d33faa116de351
carlsylvan <carlsylvan@gmail.com>
2022-11-10 11:17:42 +0100

    fixed active and removed filter

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,11 @@
 import {Task} from "./models/task"
 
-// let task1 = new Task(1, "Hugga ved", "unfinished");
-// let task2 = new Task(2, "Ladda kaminen", "unfinished");
-// let task3 = new Task(3, "Tänd brasan", "unfinished");
-// let task4 = new Task(4, "Lägg till en grej", "unfinished");
+let task1 = new Task(false, "Hugga ved", "unfinished");
+let task2 = new Task(false, "Ladda kaminen", "unfinished");
+let task3 = new Task(false, "Tänd brasan", "unfinished");
 
 
-let task1 = new Task(false, "Add new task", "unfinished");
-
-let listItems = [task1];
+let listItems = [];
 let addButton = document.getElementById("addButton");
 
 addButton.addEventListener("click", addListItem); 
@@ -16,15 +13,14 @@ addButton.addEventListener("click", addListItem);
 function loadToLS() {
     localStorage.setItem("listItems", JSON.stringify(listItems));
 };
-window.onload = updateHtml();
 
 function loadFromLS() {
     let itemsFromLs = JSON.parse(localStorage.getItem("listItems"));
     listItems = itemsFromLs.map((mappedList) => {
         return new Task(mappedList.checked, mappedList.taskDescription, mappedList.status);
     })
-    
 }
+
 function updateHtml() {
     document.getElementById("taskList").innerHTML = "";
     document.getElementById("removedList").innerHTML = "";
@@ -32,34 +28,38 @@ function updateHtml() {
         return listItems.status === "removed";
     })
 
-    for (let i = 0; i < removedList.length; i++) {
-        let newLi = document.createElement("li");
-        newLi.innerHTML = removedList[i].taskDescription;
-        document.getElementById("removedList").appendChild(newLi);
-        newLi.addEventListener("click", () => removedList[i].checked = true);
-        newLi.addEventListener("click", () => removedList[i].status = "unfinished");
-        console.log(removedList);
-    }
-
     let activeList = listItems.filter(listItems => {
         return listItems.status === "unfinished";
     })
-
+    // let checkedList = listItems.filter(listItems => {
+    //     return listItems.status === "checked";
+    // })
     for (let i = 0; i < activeList.length; i++) {
         let newLi = document.createElement("li");
         let removeButton = document.createElement("button");
         // let removedListItems = document.createElement("li");
         removeButton.innerHTML = "Remove";
-        newLi.innerHTML = listItems[i].taskDescription;
+        newLi.innerHTML = activeList[i].taskDescription;
         document.getElementById("taskList").appendChild(newLi);
         document.getElementById("taskList").append(removeButton);
         // document.getElementById("removedList").appendChild(removedListItems);
-        newLi.addEventListener("click", () => listItems[i].checked = true);
-        newLi.addEventListener("click", () => listItems[i].status = "finished");
-        removeButton.addEventListener("click", () => listItems[i].status = "removed");
+        removeButton.addEventListener("click", () => activeList[i].status = "removed");
         removeButton.addEventListener("click", () => updateHtml());
         removeButton.addEventListener("click", () => loadToLS());
+
     }
+    for (let i = 0; i < removedList.length; i++) {
+        let newLi = document.createElement("li");
+        newLi.innerHTML = removedList[i].taskDescription;
+        document.getElementById("removedList").appendChild(newLi);
+        console.log(removedList);
+    }
+    // for (let i = 0; i < checkedList.length; i++) {
+    //     let newLi = document.createElement("li");
+    //     newLi.innerHTML = removedList[i].taskDescription;
+    //     document.getElementById("listItems").appendChild(newLi);
+
+    // }
 } 
 
 function addListItem() {
@@ -70,12 +70,9 @@ function addListItem() {
             alert("Skriv en uppgift!");
         } else {
             listItems.push(newListItem);
-            for (let i = 0; i < listItems.length; i++) {
-                loadToLS();
-                loadFromLS();
-                updateHtml();
-            }
-        }      console.log(listItems);
+            loadToLS();
+            updateHtml();
+        }
  
 }
 
@@ -83,10 +80,23 @@ function inputClear(){
     document.getElementById("myInput").value = "";
 }
 
+
+function firstLoad() {
+    let itemsFromLs = JSON.parse(localStorage.getItem("listItems"));
+    if (itemsFromLs === null) {
+        listItems = [task1, task2, task3];
+        updateHtml()
+    } else {
+        loadFromLS();
+        updateHtml();
+    }
+}
+firstLoad();
+
 // Uppdatera classen Task (listitems i localStorage) till att ha fler värden (t.ex finished/inte), ha listItems i localstorage. När klicka ta bort, ändra i listitem från Unfinished till Removed.
 
 
-//Gammal html-update med remove-knapp:
+// Gammal html-update med remove-knapp:
 // function updateHtml() {
 //     document.getElementById("taskList").innerHTML = "";
 //     for (let i = 0; i < listItems.length; i++) {
